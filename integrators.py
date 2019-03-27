5dd3acc30924cc25f678c006f62c646e319b40b4
Unknown <linus.hein@cs.tum.edu>
2019-03-27 17:16:40 +0100

    Added modified versions of explicit Euler and Heun.
    Separated the calculation of the derivatives. (TODO: generalize to n dimensional derivative-functions)

--- integrators.py
+++ integrators.py
@@ -15,8 +15,7 @@ class ExplicitEuler(h.Integrator):
 
         return data
 
-
-class ExplicitHeun(h.Integrator):
+class ModifiedExplicitEuler(h.Integrator):
     def __init__(self, state, func, delta_t):
         super().__init__(state, self.stepper)
         self.f = func
@@ -25,6 +24,21 @@ class ExplicitHeun(h.Integrator):
     def stepper(self, state):
         data = state.get_vars()
 
+        derivative = self.f(data)
+        data[0] = (data + self.delta_t * derivative)[0]
+
+        data[1] = (data + self.delta_t * derivative)[1]
+
+        return data
+
+
+class ExplicitHeun(h.Integrator):
+    def __init__(self, state, func, delta_t):
+        super().__init__(state, self.stepper)
+        self.f = func
+        self.delta_t = delta_t
+
+    def helper(self, data):
         derivative_initial = self.f(data)
         pred_first = data + self.delta_t * derivative_initial
 
@@ -33,3 +47,34 @@ class ExplicitHeun(h.Integrator):
         pred_out = data + 0.5 * self.delta_t * (derivative_initial + derivative_additional)
 
         return pred_out
+
+    def stepper(self, state):
+        data = state.get_vars()
+
+        data = self.helper(data)
+
+        return data
+
+class ModifiedExplicitHeun(h.Integrator):
+    def __init__(self, state, func, delta_t):
+        super().__init__(state, self.stepper)
+        self.f = func
+        self.delta_t = delta_t
+
+    def helper(self, data):
+        derivative_initial = self.f(data)
+        pred_first = data + self.delta_t * derivative_initial
+
+        derivative_additional = self.f(pred_first)
+
+        pred_out = data + 0.5 * self.delta_t * (derivative_initial + derivative_additional)
+
+        return pred_out
+
+    def stepper(self, state):
+        data = state.get_vars()
+
+        data[0] = self.helper(data)[0]
+        data[1] = self.helper(data)[1]
+
+        return data
