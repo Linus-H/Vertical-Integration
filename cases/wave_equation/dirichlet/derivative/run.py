Fixed calculation error for wave equation implementation (dirichlet using first order derivatives).

cases/wave_equation/dirichlet/derivative/run.py
@@ -4,7 +4,6 @@ import starting_conditions
 from cases import run_utils
 from integrators.RungeKutta import Explicit
 from cases.wave_equation.dirichlet.derivative.derivative import WaveEquationDerivative
-from cases.wave_equation.dirichlet.derivative.solution import StandingWaveFixedEnd
 from utils import State
 
 c = 2.0
@@ -27,8 +26,8 @@ state = State(2, num_grid_points, axes, [("x", "u"), ("x", "v")])
 state_vars = state.get_state_vars()
 starting_cond = starting_conditions.GaussianBump(params['domain_size'] * 0.5, 50)
 
-#state_vars[0] = starting_cond.get_start_condition(axes[0])
-state_vars[0] = np.sin(axes[0] * 2 * np.pi / params['domain_size'])
+state_vars[0] = starting_cond.get_start_condition(axes[0])
+#state_vars[0] = np.sin(axes[0] * 2 * np.pi / params['domain_size'])
 
 run_utils.run_visual_without_solution(params, Explicit,
                                       WaveEquationDerivative, time_derivative_input,